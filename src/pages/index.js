2153e488cd68f613644dcdff32cff7317d2aa1c8
Ojasw Upadhyay <46334571+oupadhyay@users.noreply.github.com>
2023-05-12 19:42:59 -0700

    add analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,16 @@ import * as React from "react";
 
 // load fonts and css
 import "@carbonplan/components/globals.css";
-// import "@carbonplan/components/fonts.css";
 
 // components
 import Layout from "../components/layout";
-
 import Intro from "../components/resume/intro";
 import CourseWork from "../components/resume/course";
 import Experience from "../components/resume/experience";
 import Projects from "../components/resume/projects";
 
 import { Box } from "@theme-ui/components";
+import { Analytics } from "@vercel/analytics/react";
 
 // markup
 const AboutPage = () => {
@@ -24,6 +23,7 @@ const AboutPage = () => {
         <CourseWork />
         <Projects />
       </Box>
+      <Analytics />
     </Layout>
   );
 };
